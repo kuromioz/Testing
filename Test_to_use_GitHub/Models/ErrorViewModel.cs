0498116d6023f5b91faddd76beefcaaaa7acb472
GAM MIO KIT <TP044857@mail.apu.edu.my>
2019-07-10 11:26:49 +0800

    changes maded

---
+++ Test_to_use_GitHub/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Test_to_use_GitHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
